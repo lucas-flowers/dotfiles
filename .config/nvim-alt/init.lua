745859c07c63ab3083538431b55d0e2644cea0d1
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2025-03-24 12:37:56 -0400

    Pull in 10-ui

--- .config/nvim-alt/init.lua
+++ .config/nvim-alt/init.lua
@@ -4,25 +4,18 @@ vim.g.maplocalleader = ','
 -- Remap '\' to ',' to keep reverse character search
 vim.keymap.set('n', '\\', ',', { noremap = true, silent = true })
 
-
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+--- Options: help vim.opt                                                   ---
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
--- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
 
--- Enable mouse mode, can be useful for resizing splits for example!
+-- Mouse support in terminals
 vim.opt.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
