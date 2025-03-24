171094506948456701471f283ae1fec1da838a8a
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2025-03-24 11:20:29 -0400

    Pull in 00-general.vim to nvim-alt config

--- .config/nvim-alt/init.lua
+++ .config/nvim-alt/init.lua
@@ -28,10 +28,7 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+-- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can increase startup-time.
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
@@ -42,7 +39,7 @@ vim.opt.breakindent = true
 -- Save undo history
 vim.opt.undofile = true
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+-- Searches are case-insensitive *except* when there is an uppercase letter
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
