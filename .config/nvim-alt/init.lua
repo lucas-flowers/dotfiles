052f8203f0ad0e1cb3545a1d404c1b94841fec91
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2025-03-25 13:38:31 -0400

    Add gopls and update lazy lock

--- .config/nvim-alt/init.lua
+++ .config/nvim-alt/init.lua
@@ -577,7 +577,12 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {
+          gofumpt = true,
+          ['ui.completion.usePlaceholders'] = true,
+          ['completeUnimported'] = true,
+          ['deepCompletion'] = true,
+        },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
