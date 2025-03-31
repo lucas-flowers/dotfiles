5210d51305c45fd1c484a5b29da9320c6a19c7ab
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2025-03-31 19:07:19 -0400

    Add neotree

--- .config/nvim-alt/init.lua
+++ .config/nvim-alt/init.lua
@@ -959,6 +959,23 @@ require('lazy').setup({
   },
 
   {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      -- fill any relevant options here
+    },
+  },
+
+  {
     'folke/trouble.nvim',
     opts = {}, -- for default options, refer to the configuration section for custom setup.
     cmd = 'Trouble',
