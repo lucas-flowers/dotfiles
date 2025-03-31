c27c4282e3ff8d67bdedd447b6fa1a2a9d1ae682
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2025-03-31 19:12:41 -0400

    Set hotkey for neotree

--- .config/nvim-alt/init.lua
+++ .config/nvim-alt/init.lua
@@ -968,11 +968,9 @@ require('lazy').setup({
       -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     lazy = false, -- neo-tree will lazily load itself
-    ---@module "neo-tree"
-    ---@type neotree.Config?
-    opts = {
-      -- fill any relevant options here
-    },
+    config = function()
+      vim.api.nvim_set_keymap('n', '<leader>n', ':Neotree position=right toggle<CR>', { noremap = true, silent = true })
+    end,
   },
 
   {
