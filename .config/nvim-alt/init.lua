8bb6e78c5646f26b76feb889dc60c5566e054b5d
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2025-03-31 20:25:16 -0400

    Add terraformls

--- .config/nvim-alt/init.lua
+++ .config/nvim-alt/init.lua
@@ -616,6 +616,8 @@ require('lazy').setup({
           },
         },
 
+        terraformls = {},
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
