3dc2c1521f1d1c681fadc088b00da8e5392131ed
Lucas Flowers <31171608+lucas-flowers@users.noreply.github.com>
2025-04-01 20:05:33 -0400

    Add ansible-vim

--- .config/nvim-alt/init.lua
+++ .config/nvim-alt/init.lua
@@ -860,6 +860,11 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  -- Vim needs help detecting Ansible files
+  {
+    'pearofducks/ansible-vim',
+  },
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
